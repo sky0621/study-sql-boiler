9e6e06682f8d3da4f6ef6a521820edd2cfcebbb1
sky0621 <sky14work@gmail.com>
2020-04-14 01:47:15 +0900

    example

--- example/cmd/main.go
+++ example/cmd/main.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/volatiletech/sqlboiler/boil"
 
+	. "github.com/volatiletech/sqlboiler/queries/qm"
+
 	. "github.com/sky0621/study-sql-boiler/example/models"
 
 	_ "github.com/lib/pq"
@@ -32,6 +34,7 @@ func main() {
 	ctx := context.Background()
 
 	{
+		// SELECT COUNT(*) FROM "pilots";
 		cnt, err := Pilots().Count(ctx, db)
 		if err != nil {
 			panic(err)
@@ -39,4 +42,36 @@ func main() {
 		fmt.Println(cnt)
 	}
 
+	{
+		// SELECT * FROM "pilots" LIMIT 5;
+		results, err := Pilots(Limit(5)).All(ctx, db)
+		check(err)
+		fmt.Println(results)
+	}
+
+	{
+		// DELETE FROM "pilots" WHERE ("pilots"."id" = $1);
+		// [1]
+		cnt, err := Pilots(PilotWhere.ID.EQ(1)).DeleteAll(ctx, db)
+		check(err)
+		fmt.Println(cnt)
+	}
+
+	{
+		var res *Pilot
+		// SELECT * FROM "pilots";
+		err := NewQuery(From(TableNames.Pilots)).Bind(ctx, db, res)
+		check(err)
+		fmt.Println(res)
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("no records")
+		} else {
+			panic(err)
+		}
+	}
 }
--- example/go.mod
+++ example/go.mod
@@ -5,7 +5,9 @@ go 1.13
 require (
 	github.com/DATA-DOG/go-sqlmock v1.4.1 // indirect
 	github.com/friendsofgo/errors v0.9.2
+	github.com/kat-co/vala v0.0.0-20170210184112-42e1d8b61f12
 	github.com/lib/pq v1.3.0
 	github.com/sky0621/study-sql-boiler v0.0.0-20200411145127-21f2e4b0ac29
+	github.com/spf13/viper v1.6.3
 	github.com/volatiletech/sqlboiler v3.7.0+incompatible
 )
